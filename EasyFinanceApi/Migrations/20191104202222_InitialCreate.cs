ad347ccf4d576dbe7f718e462dc4166e48d2f27a
Daniel Jimenez C <danieljimenezcanales@gmail.com>
2019-11-04 15:25:35 -0500

    Test DbContext & Migration with Sql Server

---
+++ EasyFinanceApi/Migrations/20191104202222_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyFinanceApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Description = table.Column<string>(maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tests",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Prueba 01", "Test 01" },
+                    { 2, "Prueba 02", "Test 02" },
+                    { 3, "Prueba 03", "Test 03" },
+                    { 4, "Prueba 04", "Test 04" },
+                    { 5, "Prueba 05", "Test 05" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tests");
+        }
+    }
+}
--- EasyFinanceApi/Persistence/Context/AppDbContext.cs
+++ EasyFinanceApi/Persistence/Context/AppDbContext.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using EasyFinanceApi.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EasyFinanceApi.Persistence.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
+        public DbSet<Test> Tests { get; set; }
 
-        //public db aca
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Test>().HasData (
+                new Test { Id = 1, Description = "Prueba 01", Name = "Test 01" },
+                new Test { Id = 2, Description = "Prueba 02", Name = "Test 02" },
+                new Test { Id = 3, Description = "Prueba 03", Name = "Test 03" },
+                new Test { Id = 4, Description = "Prueba 04", Name = "Test 04" },
+                new Test { Id = 5, Description = "Prueba 05", Name = "Test 05" }
+            );
+        }
     }
 }
